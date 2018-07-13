4b9b708ac7c15f5d9ebf2e2c55a47ba4737b94de
Roger Liao <rogerliao@google.com>
2018-07-13 19:02:12 +0800

    Rename libqcodec2 module name due to duplicated
    libqcodec2 Re-defined
    
    Bug: 111163725
    Change-Id: I8f75778a01af0723c339abc96a03dc44492c89e9
    Signed-off-by: Roger Liao <rogerliao@google.com>

--- device.mk
+++ device.mk
@@ -395,7 +395,7 @@ PRODUCT_PACKAGES += \
 # Enable Codec 2.0
 PRODUCT_PACKAGES += \
     libmedia_codecserviceregistrant \
-    libqcodec2 \
+    libqcodec2_sdm710 \
     libstagefright_ccodec \
     vendor.qti.media.c2@1.0-service \
 
