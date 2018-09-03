9aa1ecef45e8b8b3f9cbbca5ccefe955729bfade
razorloves <razorloves@gmail.com>
2018-09-02 23:30:11 -0600

    b4s4: Set OPA property to enable google assistant
    Change-Id: I741171219d64a28d56531f063f95f3f6f6cf81e6

--- device-lineage.mk
+++ device-lineage.mk
@@ -22,5 +22,8 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.telephony.euicc.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/android.hardware.telephony.euicc.xml
 
+# Google Assistant
+PRODUCT_PRODUCT_PROPERTIES += ro.opa.eligible_device=true
+
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay-lineage
