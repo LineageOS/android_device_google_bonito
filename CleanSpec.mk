8992917aff49502ca411a4f6d1df72002bc9c894
Vincent Palomares <paillon@google.com>
2018-11-08 16:23:49 -0800

    thermal HAL: clean up older 1.1 HAL binary
    Test: build
    Change-Id: Ibb97211a516a32d5da9ed74f2d86d0196199c9dd
    Signed-off-by: Vincent Palomares <paillon@google.com>

--- CleanSpec.mk
+++ CleanSpec.mk
@@ -147,3 +147,4 @@ $(call add-clean-step, rm -rf $(PRODUCT_OUT)/system/etc/libnfc-nci.conf)
 
 # ThermalHAL 1.1
 $(call add-clean-step, rm -rf $(PRODUCT_OUT)/vendor/etc/init/android.hardware.thermal@1.1-service.bonito.rc)
+$(call add-clean-step, rm -rf $(PRODUCT_OUT)/vendor/bin/hw/android.hardware.thermal@1.1-service.bonito)
