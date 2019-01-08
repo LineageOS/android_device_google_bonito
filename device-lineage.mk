d9b087cab2453bda052b6b9eac3839d07c6c0d8e
Nolen Johnson <johnsonnolen@gmail.com>
2019-01-08 21:23:57 +0000

    b4s4: Define higher aspect-ratio screen config
    * This enables the long-screen feature that allows us to stretch apps to fit
      18:9 on a per-app basis.
    
    Change-Id: Iab11cf6edb6a80e1946ca7df0f321c70c2b2f163

--- device-lineage.mk
+++ device-lineage.mk
@@ -1,3 +1,5 @@
+DEVICE_PACKAGE_OVERLAYS += device/google/bonito/overlay-lineage
+
 # EUICC
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.telephony.euicc.xml:system/etc/permissions/android.hardware.telephony.euicc.xml
