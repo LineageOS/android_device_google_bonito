1a508c1853a56adb995b4c4f9671e5c071f6e4f4
Rashed Abdel-Tawab <rashed@linux.com>
2019-09-07 11:13:09 -0700

    b4s4: Build a disabled vbmeta image
    Change-Id: Ie8f1e0929b037b35b5e45c2e31be9253fa0bb097

--- BoardConfigLineage.mk
+++ BoardConfigLineage.mk
@@ -22,3 +22,7 @@ TARGET_KERNEL_CLANG_COMPILE := true
 TARGET_KERNEL_CONFIG := lineageos_b4s4_defconfig
 TARGET_KERNEL_SOURCE := kernel/google/msm-4.9
 TARGET_NEEDS_DTBOIMAGE := true
+
+# Verified Boot
+BOARD_AVB_MAKE_VBMETA_IMAGE_ARGS += --set_hashtree_disabled_flag
+BOARD_AVB_MAKE_VBMETA_IMAGE_ARGS += --flags 2
