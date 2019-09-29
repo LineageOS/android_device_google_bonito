04e7b3b5f5a28352276fea5cd867aa3d2f0adf79
Rashed Abdel-Tawab <rashed@linux.com>
2019-09-29 01:50:28 -0400

    b4s4: Drop appended DTB
    We use dtb.img in boot.img now
    
    Change-Id: Ib8b541f32e483444e42bc2231fcd1f75cfa1aab5

--- BoardConfigLineage.mk
+++ BoardConfigLineage.mk
@@ -3,7 +3,7 @@
 BOARD_AVB_MAKE_VBMETA_IMAGE_ARGS += --set_hashtree_disabled_flag
 BOARD_AVB_MAKE_VBMETA_IMAGE_ARGS += --flags 2
 
-BOARD_KERNEL_IMAGE_NAME := Image.lz4-dtb
+BOARD_KERNEL_IMAGE_NAME := Image.lz4
 TARGET_COMPILE_WITH_MSM_KERNEL := true
 TARGET_KERNEL_ADDITIONAL_FLAGS := \
     DTC=$(shell pwd)/prebuilts/misc/$(HOST_OS)-x86/dtc/dtc \
