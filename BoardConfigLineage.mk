95164216342e5e90ce59417f08e0756db2d72140
Dan Pasanen <dan.pasanen@gmail.com>
2019-10-16 22:14:11 -0500

    [DO NOT MERGE] bonito: selinux permissive
    Change-Id: I7770c4ebbc369ffc266f7d310c7d105d92425c0b

--- BoardConfigLineage.mk
+++ BoardConfigLineage.mk
@@ -12,5 +12,6 @@ TARGET_KERNEL_ARCH := arm64
 TARGET_KERNEL_CLANG_COMPILE := true
 TARGET_KERNEL_CONFIG := lineageos_bonito_defconfig
 TARGET_KERNEL_SOURCE := kernel/google/msm-4.9
+BOARD_KERNEL_CMDLINE += androidboot.selinux=permissive
 
 -include vendor/google/bonito/BoardConfigVendor.mk
