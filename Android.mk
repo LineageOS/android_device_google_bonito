5d30554aaf1f427f08aafc33f89b8ed5135257b4
Dan Pasanen <dan.pasanen@gmail.com>
2019-10-21 00:44:21 -0500

    bonito: move libdmengine symlink to product
    * i mean, that's where the blob is...
    
    Change-Id: I848039dad3f2464ba0e8a39e88021542da5724b1

--- Android.mk
+++ Android.mk
@@ -45,7 +45,7 @@ $(DM_SYMLINKS): $(LOCAL_INSTALLED_MODULE)
 	@echo "DMService lib link: $@"
 	@mkdir -p $(dir $@)
 	@rm -rf $@
-	$(hide) ln -sf /system/lib/$(notdir $@) $@
+	$(hide) ln -sf /product/lib/$(notdir $@) $@
 
 ALL_DEFAULT_INSTALLED_MODULES += $(DM_SYMLINKS)
 endif
