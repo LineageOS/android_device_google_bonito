30605a305487481b1279c99e8f226b138b883ee8
Dan Pasanen <dan.pasanen@gmail.com>
2019-11-01 07:19:07 -0500

    b4s4: exclude lineage-sdk overlays from RRO
    * lineage-sdk overlays don't work with RRO
    
    Change-Id: Iff826faf1864388066a488acc9682636b98d1098

--- device-lineage.mk
+++ device-lineage.mk
@@ -12,6 +12,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
     media.mediadrmservice.enable=true
 
 DEVICE_PACKAGE_OVERLAYS += device/google/bonito/overlay-lineage
+PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += device/google/bonito/overlay-lineage/lineage-sdk
 
 # EUICC
 PRODUCT_COPY_FILES += \
