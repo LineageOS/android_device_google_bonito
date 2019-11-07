fa1fc3ec7862591dc0c53a462246844a7d7022c9
Dan Pasanen <dan.pasanen@gmail.com>
2019-11-07 17:20:26 -0600

    b4s4: append manifests
    * This allows us to add manifests in our BoardConfigLineage.mk
    
    Change-Id: Iccfa1f52c9456712ecbd1a75e969c6139ca86d68

--- BoardConfig-common.mk
+++ BoardConfig-common.mk
@@ -186,9 +186,9 @@ TARGET_USES_COLOR_METADATA := true
 TARGET_USES_DRM_PP := true
 
 # Vendor Interface Manifest
-DEVICE_MANIFEST_FILE := device/google/bonito/manifest.xml
-DEVICE_MATRIX_FILE := device/google/bonito/compatibility_matrix.xml
-DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := device/google/bonito/device_framework_matrix.xml
+DEVICE_MANIFEST_FILE += device/google/bonito/manifest.xml
+DEVICE_MATRIX_FILE += device/google/bonito/compatibility_matrix.xml
+DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE += device/google/bonito/device_framework_matrix.xml
 
 # Userdebug only Vendor Interface Manifest
 ifneq (,$(filter eng, $(TARGET_BUILD_VARIANT)))
