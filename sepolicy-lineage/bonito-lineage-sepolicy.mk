66e587adbedc52faba86ab901d5442989726c1ea
Dan Pasanen <dan.pasanen@gmail.com>
2019-11-07 17:42:05 -0600

    b4s4: sepolicies for bfq goodies
    * allow vendor_init to setprop for system_prop
    * allow init to relabel sysfs_mmc files and dirs
    
    Change-Id: Ibfb6c849a92f532035bbd95634d310c3700e6d53

--- sepolicy-lineage/bonito-lineage-sepolicy.mk
+++ sepolicy-lineage/bonito-lineage-sepolicy.mk
@@ -1,2 +1,3 @@
+BOARD_SEPOLICY_DIRS += device/google/bonito/sepolicy-lineage/common
 BOARD_SEPOLICY_DIRS += device/google/bonito/sepolicy-lineage/dynamic
 BOARD_SEPOLICY_DIRS += device/google/bonito/sepolicy-lineage/vendor
