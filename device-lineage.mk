b07c91b8312227f4a336b3eda449281f601b1b59
Dan Pasanen <dan.pasanen@gmail.com>
2019-11-07 08:36:07 -0600

    b4s4: add livedisplay 2.0 hal
    Change-Id: I1d5ee7c88d5ba2de57f963e748f2d34def042b3f

--- device-lineage.mk
+++ device-lineage.mk
@@ -8,6 +8,11 @@ DEVICE_PACKAGE_OVERLAYS += device/google/bonito/overlay-lineage
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.telephony.euicc.xml:system/etc/permissions/android.hardware.telephony.euicc.xml
 
+# LiveDisplay
+PRODUCT_PACKAGES += \
+    vendor.lineage.livedisplay@2.0-service-sdm \
+    vendor.lineage.livedisplay@2.0-service-sysfs
+
 # RCS
 PRODUCT_PACKAGES += \
     com.android.ims.rcsmanager \
