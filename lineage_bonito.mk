daa0d000788bc404ef59bc42cdae4f8aa1253d87
Rashed Abdel-Tawab <rashed@linux.com>
2019-11-20 10:22:06 -0500

    b4s4: Initial Lineage bringup
    Change-Id: Ieddd0460eeb223512b5d3ce9504f6a654d44c0d9

---
+++ lineage_bonito.mk
@@ -0,0 +1,28 @@
+#
+# Copyright (C) 2020-2021 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit some common Lineage stuff.
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+
+# Inherit device configuration
+$(call inherit-product, device/google/bonito/aosp_bonito.mk)
+
+include device/google/bonito/device-lineage.mk
+
+# Device identifier. This must come after all inclusions
+PRODUCT_BRAND := google
+PRODUCT_MODEL := Pixel 3a XL
+PRODUCT_NAME := lineage_bonito
+
+# Boot animation
+TARGET_SCREEN_HEIGHT := 2160
+TARGET_SCREEN_WIDTH := 1080
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    TARGET_PRODUCT==bonito \
+    PRIVATE_BUILD_DESC="bonito-user 12 SQ1A.211205.008 7888514 release-keys"
+
+BUILD_FINGERPRINT := google/bonito/bonito:12/SQ1A.211205.008/7888514:user/release-keys
