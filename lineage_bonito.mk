26e67787acc7f0812bd8ace375d5907052f36153
Rashed Abdel-Tawab <rashed@linux.com>
2019-11-20 10:22:06 -0500

    b4s4: Initial Lineage bringup
    Change-Id: Ieddd0460eeb223512b5d3ce9504f6a654d44c0d9

---
+++ lineage_bonito.mk
@@ -0,0 +1,24 @@
+# Boot animation
+TARGET_SCREEN_HEIGHT := 2960
+TARGET_SCREEN_WIDTH := 1440
+
+# Inherit some common Lineage stuff.
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+
+# Inherit device configuration
+$(call inherit-product, device/google/bonito/aosp_bonito.mk)
+
+-include device/google/bonito/device-lineage.mk
+
+## Device identifier. This must come after all inclusions
+PRODUCT_NAME := lineage_bonito
+PRODUCT_MODEL := Pixel 4 XL
+PRODUCT_BRAND := google
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRODUCT_NAME=bonito \
+    PRIVATE_BUILD_DESC="bonito-user 11 RQ1A.210105.002 6985033 release-keys"
+
+BUILD_FINGERPRINT := google/bonito/bonito:11/RQ1A.210105.002/6985033:user/release-keys
+
+$(call inherit-product-if-exists, vendor/google/bonito/bonito-vendor.mk)
