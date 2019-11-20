ddbdeca70c841dba724211a522400bde960bb4db
Rashed Abdel-Tawab <rashed@linux.com>
2019-11-20 10:22:06 -0500

    b4s4: Initial Lineage bringup
    Change-Id: Ieddd0460eeb223512b5d3ce9504f6a654d44c0d9

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -18,8 +18,12 @@ PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/aosp_bonito.mk \
     $(LOCAL_DIR)/aosp_sargo.mk \
     $(LOCAL_DIR)/aosp_bonito_hwasan.mk \
-    $(LOCAL_DIR)/aosp_sargo_hwasan.mk
+    $(LOCAL_DIR)/aosp_sargo_hwasan.mk \
+    $(LOCAL_DIR)/lineage_bonito.mk \
+    $(LOCAL_DIR)/lineage_sargo.mk
 
 COMMON_LUNCH_CHOICES := \
     aosp_bonito-userdebug \
-    aosp_sargo-userdebug
+    aosp_sargo-userdebug \
+    lineage_bonito-userdebug \
+    lineage_sargo-userdebug
---
+++ lineage_sargo.mk
@@ -0,0 +1,28 @@
+#
+# Copyright (C) 2020-2021 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit some common Lineage stuff.
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+
+# Inherit device configuration
+$(call inherit-product, device/google/bonito/aosp_sargo.mk)
+
+include device/google/bonito/device-lineage.mk
+
+# Device identifier. This must come after all inclusions
+PRODUCT_BRAND := google
+PRODUCT_MODEL := Pixel 3a
+PRODUCT_NAME := lineage_sargo
+
+# Boot animation
+TARGET_SCREEN_HEIGHT := 2220
+TARGET_SCREEN_WIDTH := 1080
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    TARGET_PRODUCT=sargo \
+    PRIVATE_BUILD_DESC="sargo-user 12 SP1A.211105.002 7743617 release-keys"
+
+BUILD_FINGERPRINT := google/sargo/sargo:12/SP1A.211105.002/7743617:user/release-keys
