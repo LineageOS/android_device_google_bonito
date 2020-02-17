GrapheneOS-Archive/device_google_coral

Update dumpstate HAL to V1.1
Bug: 143184495
Test: atest VtsHalDumpstateV1_1TargetTest pass
Change-Id: I087622c49e2632d7e759a6384c5e09102e072fd7

device.mk
@@ -764,7 +764,7 @@ endif
 
 # Dumpstate HAL
 PRODUCT_PACKAGES += \
-    android.hardware.dumpstate@1.0-service.coral
+    android.hardware.dumpstate@1.1-service.coral
 
 # Citadel
 PRODUCT_PACKAGES += \

dumpstate/Android.mk
@@ -16,8 +16,8 @@
 
 LOCAL_PATH:= $(call my-dir)
 include $(CLEAR_VARS)
-LOCAL_MODULE := android.hardware.dumpstate@1.0-service.coral
-LOCAL_INIT_RC := android.hardware.dumpstate@1.0-service.coral.rc
+LOCAL_MODULE := android.hardware.dumpstate@1.1-service.coral
+LOCAL_INIT_RC := android.hardware.dumpstate@1.1-service.coral.rc
 LOCAL_MODULE_RELATIVE_PATH := hw
 
 LOCAL_SRC_FILES := \
@@ -26,6 +26,7 @@ LOCAL_SRC_FILES := \
 
 LOCAL_SHARED_LIBRARIES := \
     android.hardware.dumpstate@1.0 \
+    android.hardware.dumpstate@1.1 \
     libbase \
     libcutils \
     libdumpstateutil \

LineageOS/android_device_google_bonito

device.mk
@@ -690,7 +690,7 @@ endif
 
 # Dumpstate HAL
 PRODUCT_PACKAGES += \
-    android.hardware.dumpstate@1.0-service.bonito
+    android.hardware.dumpstate@1.1-service.bonito
 
 # Citadel
 PRODUCT_PACKAGES += \

dumpstate/Android.mk
@@ -16,8 +16,8 @@
 
 LOCAL_PATH:= $(call my-dir)
 include $(CLEAR_VARS)
-LOCAL_MODULE := android.hardware.dumpstate@1.0-service.bonito
-LOCAL_INIT_RC := android.hardware.dumpstate@1.0-service.bonito.rc
+LOCAL_MODULE := android.hardware.dumpstate@1.1-service.bonito
+LOCAL_INIT_RC := android.hardware.dumpstate@1.1-service.bonito.rc
 LOCAL_MODULE_RELATIVE_PATH := hw
 
 LOCAL_SRC_FILES := \
@@ -26,6 +26,7 @@ LOCAL_SRC_FILES := \
 
 LOCAL_SHARED_LIBRARIES := \
     android.hardware.dumpstate@1.0 \
+    android.hardware.dumpstate@1.1 \
     libbase \
     libcutils \
     libdumpstateutil \