60c9e99495e634c61211c9fa205303143ada44e7
razorloves <razorloves@gmail.com>
2020-05-05 21:07:30 -0500

    b4s4: Build Snap camera app
    Change-Id: I1a0ed1e9518f6e20a264632243a30847157acc9f

--- device-lineage.mk
+++ device-lineage.mk
@@ -14,6 +14,10 @@
 # limitations under the License.
 #
 
+# Camera
+PRODUCT_PACKAGES += \
+    Snap
+
 # EUICC
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.telephony.euicc.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/android.hardware.telephony.euicc.xml
