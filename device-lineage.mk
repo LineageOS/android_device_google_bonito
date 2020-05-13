74b073ed0c07ad28c08114c79a8bf35d7b6428a4
razorloves <razorloves@gmail.com>
2020-05-12 23:50:50 -0500

    b4s4: Set slotId# of non-removable esim
    This is needed for esim to function properly.
    Without it the EID value could not be read.
    
    Change-Id: I414a7c98362523c5880cfa90b6ce9c0ce92e9c32

--- device-lineage.mk
+++ device-lineage.mk
@@ -17,3 +17,6 @@
 # EUICC
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.telephony.euicc.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/android.hardware.telephony.euicc.xml
+
+# Overlays
+DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay-lineage
