1f5619887dccf6ddaacefd72f9a1b4419ee32d66
Felipe Leme <felipeal@google.com>
2020-07-10 16:19:31 -0700

    Add option to use a custom board config through PHONE_CAR_BOARD_CONFIG.
    phone_car variants use the same board as phone, but need to customize
    some properties like screen density.
    
    Test: . ./build/envsetup.sh && lunch bonito_car-eng && m && grep ro.sf.lcd_density $OUT/vendor/build.prop
    
    Bug: 160821620
    Bug: 160823761
    
    Change-Id: I0369251767d97575c482cdb010593de32149b115

--- bonito/BoardConfig.mk
+++ bonito/BoardConfig.mk
@@ -14,9 +14,13 @@
 # limitations under the License.
 #
 
-TARGET_BOOTLOADER_BOARD_NAME := bonito
-TARGET_SCREEN_DENSITY := 400
-TARGET_RECOVERY_UI_MARGIN_HEIGHT := 165
+ifdef PHONE_CAR_BOARD_CONFIG
+  include $(PHONE_CAR_BOARD_CONFIG)
+else
+  TARGET_BOOTLOADER_BOARD_NAME := bonito
+  TARGET_SCREEN_DENSITY := 400
+  TARGET_RECOVERY_UI_MARGIN_HEIGHT := 165
 
-include device/google/bonito/BoardConfig-common.mk
--include vendor/google_devices/bonito/proprietary/BoardConfigVendor.mk
+  include device/google/bonito/BoardConfig-common.mk
+  -include vendor/google_devices/bonito/proprietary/BoardConfigVendor.mk
+endif
