5ddd99799c91330cd0184cfbd17f8aecf2503ede
Yo Chiang <yochiang@google.com>
2020-08-11 13:26:50 +0800

    Don't use on_vendor VNDK APEX for REL build
    For non-REL branch, move VNDK APEX to /vendor partition so we don't need
    to update vendor.img prebuilt everytime there is a change in the VNDK
    libraries.
    For REL branch, the API/ABI surfaces of VNDK libraries are frozen so
    don't need to move to /vendor partition.
    
    Bug: 140912145
    Test: Build on REL and non-REL branch
    Change-Id: Ie1c8dc5c78560842147d88492d7bc191c0916633

--- aosp_bonito.mk
+++ aosp_bonito.mk
@@ -51,7 +51,11 @@ PRODUCT_COPY_FILES += \
 # STOPSHIP deal with Qualcomm stuff later
 # PRODUCT_RESTRICT_VENDOR_FILES := all
 
-PRODUCT_PACKAGES += com.android.vndk.current.on_vendor
+# Keep the VNDK APEX in /system partition for REL branches as these branches are
+# expected to have stable API/ABI surfaces.
+ifneq (REL,$(PLATFORM_VERSION_CODENAME))
+  PRODUCT_PACKAGES += com.android.vndk.current.on_vendor
+endif
 
 PRODUCT_MANUFACTURER := Google
 PRODUCT_BRAND := Android
