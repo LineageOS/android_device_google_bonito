0c6d0de6c9d1e4ae3c27484db205da7d7be1493f
Nolen Johnson <johnsonnolen@gmail.com>
2020-08-18 23:11:17 -0400

    b4s4: Organize Lineage makefiles
    * Alphabetize.
    * Bring more inline with floral cherry-pick's have
      less merge conflicts.
    * Move vendor properties to vendor.prop and include.
    * Use LOCAL_PATH where possible.
    
    Change-Id: Iba815fb024596571f3f0099110e8b72a8e359c49

--- device-lineage.mk
+++ device-lineage.mk
@@ -1,15 +1,25 @@
-# Build vendor img
-AB_OTA_PARTITIONS += \
-    vendor
+#
+# Copyright (C) 2020 The LineageOS Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+# Camera
+PRODUCT_PACKAGES += Snap
 
 # Display
 PRODUCT_PACKAGES += \
     libdisplayconfig
-
-# DRM
-PRODUCT_PROPERTY_OVERRIDES += \
-    drm.service.enabled=true \
-    media.mediadrmservice.enable=true
 
 DEVICE_PACKAGE_OVERLAYS += device/google/bonito/overlay-lineage
 PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += device/google/bonito/overlay-lineage/lineage-sdk
@@ -19,26 +29,39 @@ PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += device/google/bonito/overlay/packages/a
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.telephony.euicc.xml:system/etc/permissions/android.hardware.telephony.euicc.xml
 
+# GMS
+WITH_GMS_FI := true
+
 # LiveDisplay
 PRODUCT_PACKAGES += \
     vendor.lineage.livedisplay@2.0-service-sdm
 
-# LMK
-PRODUCT_PRODUCT_PROPERTIES += \
-    ro.lmk.use_psi=true
+# Overlays
+DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay-lineage
+PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += $(LOCAL_PATH)/overlay-lineage/lineage-sdk
+PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += $(LOCAL_PATH)/overlay-lineage/packages/apps/Snap
+PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += $(LOCAL_PATH)/overlay/packages/apps/Bluetooth
+
+# Partitions
+AB_OTA_PARTITIONS += \
+    vendor
+
+# Permissions
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/permissions/privapp-permissions-aosp-extended.xml:system/etc/permissions/privapp-permissions-aosp-extended.xml
 
 # Preopt SystemUI
 PRODUCT_DEXPREOPT_SPEED_APPS += \
     SystemUI
+
+# Properties
+TARGET_VENDOR_PROP := $(LOCAL_PATH)/vendor.prop
 
 # RCS
 PRODUCT_PACKAGES += \
     com.android.ims.rcsmanager \
     PresencePolling \
     RcsService
-
-# Snap
-PRODUCT_PACKAGES += Snap
 
 # Trust HAL
 PRODUCT_PACKAGES += \
@@ -49,11 +72,6 @@ PRODUCT_PACKAGES += \
     libjson \
     libtinyxml
 
-# WiFi
+# Wi-Fi
 PRODUCT_PACKAGES += \
     libwifi-hal-qcom
-
-WITH_GMS_FI := true
-
-PRODUCT_COPY_FILES += \
-    device/google/bonito/permissions/privapp-permissions-aosp-extended.xml:system/etc/permissions/privapp-permissions-aosp-extended.xml
