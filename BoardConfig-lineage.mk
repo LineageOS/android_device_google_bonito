7b8c66625738057c1f53b7b725e1ac51e7a5e57d
Nolen Johnson <johnsonnolen@gmail.com>
2020-08-18 23:11:17 -0400

    b4s4: Organize Lineage makefiles
    * Alphabetize.
    * Bring more inline with floral cherry-pick's have
      less merge conflicts.
    * Move vendor properties to vendor.prop and include.
    * Use LOCAL_PATH where possible.
    
    Change-Id: Iba815fb024596571f3f0099110e8b72a8e359c49

--- BoardConfig-lineage.mk
+++ BoardConfig-lineage.mk
@@ -1,11 +1,20 @@
-# Common board config for bonito, sargo
+#
+# Copyright (C) 2020 The LineageOS Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
 
-# Build vendor image
-BOARD_VENDORIMAGE_FILE_SYSTEM_TYPE := ext4
-
-BOARD_AVB_MAKE_VBMETA_IMAGE_ARGS += --set_hashtree_disabled_flag
-BOARD_AVB_MAKE_VBMETA_IMAGE_ARGS += --flags 2
-
+# Kernel
 BOARD_KERNEL_IMAGE_NAME := Image.lz4
 TARGET_COMPILE_WITH_MSM_KERNEL := true
 TARGET_KERNEL_ADDITIONAL_FLAGS := \
@@ -16,16 +25,26 @@ TARGET_KERNEL_CLANG_COMPILE := true
 TARGET_KERNEL_CONFIG := lineageos_bonito_defconfig
 TARGET_KERNEL_SOURCE := kernel/google/msm-4.9
 
-DEVICE_MANIFEST_FILE += device/google/bonito/lineage_manifest.xml
+# Manifests
 DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE += device/google/bonito/lineage_compatibility_matrix.xml
+DEVICE_MANIFEST_FILE += device/google/bonito/lineage_manifest.xml
 
+# Partitions
+BOARD_VENDORIMAGE_FILE_SYSTEM_TYPE := ext4
+
+# Reserved Space
 ifneq ($(WITH_GMS),true)
     BOARD_PRODUCTIMAGE_PARTITION_RESERVED_SIZE := 15728640
     BOARD_SYSTEMIMAGE_PARTITION_RESERVED_SIZE := 1887436800
     BOARD_VENDORIMAGE_PARTITION_RESERVED_SIZE := 15728640
 endif
 
+# SELinux
 BOARD_SEPOLICY_DIRS += device/google/bonito/sepolicy-lineage/dynamic
 BOARD_SEPOLICY_DIRS += device/google/bonito/sepolicy-lineage/vendor
+
+# Verified Boot
+BOARD_AVB_MAKE_VBMETA_IMAGE_ARGS += --set_hashtree_disabled_flag
+BOARD_AVB_MAKE_VBMETA_IMAGE_ARGS += --flags 2
 
 -include vendor/google/bonito/BoardConfigVendor.mk
