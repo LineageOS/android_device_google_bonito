cab247f397eb478bae733407932c386a0cabd9aa
Alexander Koskovich <zvnexus@outlook.com>
2020-12-03 10:26:15 +0000

    b4s4: Disable the usage of ConfigStore.
     * As per commit https://github.com/LineageOS/android_hardware_interfaces/commit/11a113a67e87a8da9a9bf0a73883f2325de90934 ("Add disable_configstore").
    
    [SebaUbuntu] Remove manifest entry as well
    
    Change-Id: I5482989a8a96227a3e2a5317eacf613c641bfa8e

--- device-lineage.mk
+++ device-lineage.mk
@@ -73,6 +73,10 @@ PRODUCT_PACKAGES += \
     libcodec2_vndk.vendor \
     libstagefright_bufferpool@2.0.1.vendor
 
+# Configstore
+PRODUCT_PACKAGES += \
+    disable_configstore
+
 # Confirmation UI
 PRODUCT_PACKAGES += \
     android.hardware.confirmationui@1.0.vendor:64 \
