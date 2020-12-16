cbee887d39288274b0d9f9157afa6cfb1d69b8b1
hkuang <hkuang@google.com>
2020-12-15 17:34:57 -0800

    bonito: Update the transcoding operating rate for bonito.
    Bug: 175231451
    Test: CtsMediaTranscodingTestCases:MediaTranscodeManagerTest
    Change-Id: I53188b2e0eee74e0f3eafc733335aa21d4ed348b

--- device.mk
+++ device.mk
@@ -458,9 +458,11 @@ PRODUCT_PROPERTY_OVERRIDES += \
 PRODUCT_PROPERTY_OVERRIDES += \
     debug.stagefright.c2inputsurface=-1 \
 
-#  Maximum operating rate for codecs.
+# Transcoding related property.
 PRODUCT_PROPERTY_OVERRIDES += \
-    debug.media.transcoding.codec_max_operating_rate=120 \
+    debug.media.transcoding.codec_max_operating_rate_720P=240 \
+    debug.media.transcoding.codec_max_operating_rate_1080P=120 \
+    debug.media.transcoding.codec_max_operating_rate_4K=50 \
 
 PRODUCT_PACKAGES += \
     libqcodec2 \
