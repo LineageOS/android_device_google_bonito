4e27d46d6af3313dc85c905e0c78431aa4591f88
Eamon Powell <eamonpowell@outlook.com>
2020-12-29 10:16:40 +1100

    b4s4: Add missing properties and packages
    Now that we are building our own vendor image, some necessary
    packages aren't being built and some props aren't being set.
    Additionally add missing components from system_ext.
    
    Co-authored-by: Josh Chasky <jchasky@gmail.com>
    Co-authored-by: Eamon Powell <eamonpowell@outlook.com>
    Change-Id: I3e91822b336761cdecf3f6ebe416276dc0585785

--- device-lineage.mk
+++ device-lineage.mk
@@ -14,6 +14,52 @@
 # limitations under the License.
 #
 
+# Build necessary packages for system
+PRODUCT_PACKAGES += \
+    libmediaplayerservice \
+    libstagefright_httplive:64
+
+# Build necessary packages for vendor
+PRODUCT_PACKAGES += \
+    chre \
+    ese_spi_nxp:64 \
+    hardware.google.light@1.0.vendor \
+    libavservices_minijail_vendor:32 \
+    libcodec2_hidl@1.0.vendor:32 \
+    libcodec2_vndk.vendor \
+    libcppbor.vendor:64 \
+    libdisplayconfig \
+    libdrm.vendor \
+    libhidltransport.vendor \
+    libhwbinder.vendor \
+    libjson \
+    libkeymaster_messages.vendor:64 \
+    libkeymaster_portable.vendor:64 \
+    libnetfilter_conntrack:64 \
+    libnfnetlink:64 \
+    libnos:64 \
+    libnos_client_citadel:64 \
+    libnos_datagram:64 \
+    libnos_datagram_citadel:64 \
+    libnosprotos:64 \
+    libnos_transport:64 \
+    libpuresoftkeymasterdevice.vendor:64 \
+    libsensorndkbridge:64 \
+    libsoft_attestation_cert.vendor:64 \
+    libteeui_hal_support.vendor:64 \
+    libtinycompress \
+    libtinyxml \
+    libwifi-hal:64 \
+    libwifi-hal-qcom \
+    nos_app_avb:64 \
+    nos_app_identity:64 \
+    nos_app_keymaster:64 \
+    nos_app_weaver:64 \
+    vendor.display.config@1.0.vendor \
+    vendor.display.config@1.1.vendor \
+    vendor.display.config@1.2.vendor \
+    vendor.display.config@1.3.vendor
+
 # Camera
 PRODUCT_PACKAGES += \
     Snap
@@ -27,3 +73,10 @@ PRODUCT_PRODUCT_PROPERTIES += ro.opa.eligible_device=true
 
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay-lineage
+
+# Properties
+PRODUCT_PROPERTY_OVERRIDES += \
+    drm.service.enabled=true  \
+    media.mediadrmservice.enable=true \
+    ro.hardware.egl=adreno \
+    ro.hardware.vulkan=adreno
