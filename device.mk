ca2bc90e82421475b223ca65ec4813c53f567945
Rajeev Kumar <rajekumar@google.com>
2021-01-14 13:43:15 -0800

    Used optional assignment syntax for ro.control_privapp_permissions, dalvik.vm.heapgrowthlimit to set the default value.
    Bug: 177587392
    Test: Built and flashed a sargo_svelte target on a Pixel 3A device.
    Change-Id: Id3ee2e8cf44a712227c05825e66166996bc0cffc

--- device.mk
+++ device.mk
@@ -52,7 +52,7 @@ PRODUCT_COPY_FILES += \
 
 # Enforce privapp-permissions whitelist
 PRODUCT_PROPERTY_OVERRIDES += \
-    ro.control_privapp_permissions=enforce
+    ro.control_privapp_permissions?=enforce
 
 # Enable on-access verification of priv apps. This requires fs-verity support in kernel.
 PRODUCT_PROPERTY_OVERRIDES += \
@@ -701,7 +701,7 @@ PRODUCT_PACKAGES += \
 
 # Override heap growth limit due to high display density on device
 PRODUCT_PROPERTY_OVERRIDES += \
-    dalvik.vm.heapgrowthlimit=256m
+    dalvik.vm.heapgrowthlimit?=256m
 
 PRODUCT_PACKAGES += \
     ipacm \
