0711d91cf0d1d97b1b9929b36b9fca9b8661a609
Lucas Dupin <dupin@google.com>
2021-08-11 19:57:41 -0700

    Allow boot color propagation
    Allows SystemUI to write the boot color sysprop
    
    Test: manual
    Bug: 190093578
    Change-Id: I215d92ee57240b74ce2636a67d0efd6f57cc9a30
    Merged-In: I215d92ee57240b74ce2636a67d0efd6f57cc9a30

--- bonito-sepolicy.mk
+++ bonito-sepolicy.mk
@@ -8,6 +8,9 @@ BOARD_VENDOR_SEPOLICY_DIRS += device/google/bonito-sepolicy/vendor/google
 BOARD_VENDOR_SEPOLICY_DIRS += device/google/bonito-sepolicy/vendor/verizon
 BOARD_VENDOR_SEPOLICY_DIRS += device/google/bonito-sepolicy/tracking_denials
 
+# system_ext
+SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += device/google/bonito-sepolicy/system_ext/private
+
 # Pixel-wide policy
 BOARD_VENDOR_SEPOLICY_DIRS += hardware/google/pixel-sepolicy/ramdump/common
 BOARD_VENDOR_SEPOLICY_DIRS += hardware/google/pixel-sepolicy/powerstats
