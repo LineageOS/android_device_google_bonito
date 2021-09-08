abb82454619fc15c955677a91705289015ff2aa0
Nolen Johnson <johnsonnolen@gmail.com>
2021-09-08 16:55:57 -0400

    b4s4: RQ3A.210905.001
    Change-Id: Ida927b6c8d85d7720861a80340e57d9b220566f1

--- lineage_bonito.mk
+++ lineage_bonito.mk
@@ -17,8 +17,8 @@ PRODUCT_BRAND := google
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME=bonito \
-    PRIVATE_BUILD_DESC="bonito-user 11 RQ3A.210805.001.A1 7474174 release-keys"
+    PRIVATE_BUILD_DESC="bonito-user 11 RQ3A.210905.001 7511028 release-keys"
 
-BUILD_FINGERPRINT := google/bonito/bonito:11/RQ3A.210805.001.A1/7474174:user/release-keys
+BUILD_FINGERPRINT := google/bonito/bonito:11/RQ3A.210905.001/7511028:user/release-keys
 
 $(call inherit-product, vendor/google/bonito/bonito-vendor.mk)
