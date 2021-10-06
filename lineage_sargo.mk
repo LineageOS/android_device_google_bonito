26110c8a4de0ec62197a9f6cb98abd8dd10c7a2d
Nolen Johnson <johnsonnolen@gmail.com>
2021-10-06 12:43:54 -0400

    b4s4: RQ3A.211001.001
    Change-Id: Ic91312e9dd304b66b91cc25bdc56c5f333fe5d84

--- lineage_sargo.mk
+++ lineage_sargo.mk
@@ -17,8 +17,8 @@ PRODUCT_BRAND := google
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME=sargo \
-    PRIVATE_BUILD_DESC="sargo-user 11 RQ3A.210905.001 7511028 release-keys"
+    PRIVATE_BUILD_DESC="sargo-user 11 RQ3A.211001.001 7641976 release-keys"
 
-BUILD_FINGERPRINT := google/sargo/sargo:11/RQ3A.210905.001/7511028:user/release-keys
+BUILD_FINGERPRINT := google/sargo/sargo:11/RQ3A.211001.001/7641976:user/release-keys
 
 $(call inherit-product, vendor/google/sargo/sargo-vendor.mk)
