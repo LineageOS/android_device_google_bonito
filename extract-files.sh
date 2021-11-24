2f706dbe9de8f51b06561bf37a27f23ecfbfef47
Michael Bestas <mkbestas@lineageos.org>
2021-11-24 23:31:19 +0200

    bonito: Add extract-files.sh helper script
    Change-Id: Ie5f4c6ab8debf1a2b11db45b0cb6957e2539ac80

---
+++ extract-files.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+#
+# Copyright (C) 2016 The CyanogenMod Project
+# Copyright (C) 2017-2020 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+set -e
+
+"./bonito/extract-files.sh" "$@"
