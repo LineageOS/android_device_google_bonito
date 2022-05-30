7bc689f07d256095950ac478fd1e7a6070503fca
Bruno Martins <bgcngm@gmail.com>
2022-05-30 19:27:51 +0100

    b4s4: Drop default DRM service
    This is not needed, since Widevine DRM and Clearkey DRM
    services are available.
    
    Change-Id: I97023f1bab88ddb8fa4cb205df190c516a759951

--- device.mk
+++ device.mk
@@ -392,8 +392,6 @@ PRODUCT_PROPERTY_OVERRIDES += \
 
 # DRM HAL
 PRODUCT_PACKAGES += \
-    android.hardware.drm@1.0-impl \
-    android.hardware.drm@1.0-service \
     android.hardware.drm@1.4.vendor \
     android.hardware.drm-service.clearkey
 
