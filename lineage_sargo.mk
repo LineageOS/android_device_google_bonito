b90b767c1f3249730ccf0286145f7d7b787cf86a
Nolen Johnson <johnsonnolen@gmail.com>
2022-09-07 22:44:17 -0400

    b4s4: SP2A.220505.008
    Change-Id: I199d32c60c4e91a5c56596160d5a7a7cb2ee683d

--- lineage_sargo.mk
+++ lineage_sargo.mk
@@ -23,8 +23,8 @@ TARGET_SCREEN_WIDTH := 1080
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     TARGET_PRODUCT=sargo \
-    PRIVATE_BUILD_DESC="sargo-user 12 SP2A.220505.006 8561491 release-keys"
+    PRIVATE_BUILD_DESC="sargo-user 12 SP2A.220505.008 8782922 release-keys"
 
-BUILD_FINGERPRINT := google/sargo/sargo:12/SP2A.220505.006/8561491:user/release-keys
+BUILD_FINGERPRINT := google/sargo/sargo:12/SP2A.220505.008/8782922:user/release-keys
 
 $(call inherit-product, vendor/google/sargo/sargo-vendor.mk)
