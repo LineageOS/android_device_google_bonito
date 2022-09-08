99719c4acd66e5224ced9a61b49f04c7f1bd079a
Nolen Johnson <johnsonnolen@gmail.com>
2022-09-07 22:44:17 -0400

    b4s4: SP2A.220505.008
    Change-Id: I199d32c60c4e91a5c56596160d5a7a7cb2ee683d

--- lineage_bonito.mk
+++ lineage_bonito.mk
@@ -23,8 +23,8 @@ TARGET_SCREEN_WIDTH := 1080
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     TARGET_PRODUCT==bonito \
-    PRIVATE_BUILD_DESC="bonito-user 12 SP2A.220505.006 8561491 release-keys"
+    PRIVATE_BUILD_DESC="bonito-user 12 SP2A.220505.008 8782922 release-keys"
 
-BUILD_FINGERPRINT := google/bonito/bonito:12/SP2A.220505.006/8561491:user/release-keys
+BUILD_FINGERPRINT := google/bonito/bonito:12/SP2A.220505.008/8782922:user/release-keys
 
 $(call inherit-product, vendor/google/bonito/bonito-vendor.mk)
