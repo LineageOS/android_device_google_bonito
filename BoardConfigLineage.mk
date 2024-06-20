2687905bc9985258b48adf546c61a3faa9568980
Michael Bestas <mkbestas@lineageos.org>
2024-06-20 12:49:06 +0300

    bonito: Decrease /product reserved size again
    QPR3 and Gallery2 editor made the images go over the limit again.
    
    Change-Id: I9971a4e634c879d9cd019e68626265361d33e582

--- BoardConfigLineage.mk
+++ BoardConfigLineage.mk
@@ -31,7 +31,7 @@ BOARD_VENDORIMAGE_FILE_SYSTEM_TYPE := ext4
 # Reserve space for gapps install
 -include vendor/lineage/config/BoardConfigReservedSize.mk
 ifneq ($(WITH_GMS),true)
-BOARD_PRODUCTIMAGE_PARTITION_RESERVED_SIZE := 1083179008
+BOARD_PRODUCTIMAGE_PARTITION_RESERVED_SIZE := 1069547520
 endif
 
 # SELinux
