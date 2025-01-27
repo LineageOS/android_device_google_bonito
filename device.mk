384cc2bc5de76a53394af21eba53bc16bf111ca9
Michael Bestas <mkbestas@lineageos.org>
2025-01-27 12:44:55 +0330

    b4s4: Migrate mount point creation out of AndroidBoard.mk
    Change-Id: Id5986b8a740e45f864eecdd2bd82d9455f128d6b

--- device.mk
+++ device.mk
@@ -121,6 +121,10 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/init.recovery.hardware.device.rc:recovery/root/init.recovery.bonito.rc \
     $(LOCAL_PATH)/init.recovery.hardware.device.rc:recovery/root/init.recovery.sargo.rc \
 
+# Partitions
+PRODUCT_PACKAGES += \
+    vendor_firmware_mnt_mountpoint
+
 MSM_VIDC_TARGET_LIST := sdm710 # Get the color format from kernel headers
 MASTER_SIDE_CP_TARGET_LIST := sdm710 # ION specific settings
 
