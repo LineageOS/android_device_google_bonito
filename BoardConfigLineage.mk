2aa18338556cbb7758e4df931c389030e72b7a65
Nolen Johnson <johnsonnolen@gmail.com>
2025-08-18 15:18:39 -0400

    b4s4: Migrate to AIDL LiveDisplay HAL
    Change-Id: I4e8be836e94d442b40c63a1ac7dade3c9d9c784c

--- BoardConfigLineage.mk
+++ BoardConfigLineage.mk
@@ -14,10 +14,6 @@ TARGET_KERNEL_LLVM_BINUTILS := false
 TARGET_KERNEL_SOURCE := kernel/google/msm-4.9
 TARGET_NEEDS_DTBOIMAGE := true
 
-# Manifests
-DEVICE_MANIFEST_FILE += device/google/bonito/lineage_manifest.xml
-DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE += vendor/lineage/config/device_framework_matrix.xml
-
 # Partitions
 AB_OTA_PARTITIONS += \
     vendor
